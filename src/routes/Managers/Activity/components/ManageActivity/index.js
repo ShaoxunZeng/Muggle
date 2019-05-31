1bfe6a5d6dbfbed860f0f6566f0ca359c7035088
Slytherin <1085684645@qq.com>
2019-05-31 18:31:42 +0800

    feat: 完成活动管理的删除功能,完成新增活动界面

--- src/routes/Managers/Activity/components/ManageActivity/index.js
+++ src/routes/Managers/Activity/components/ManageActivity/index.js
@@ -1,15 +1,8 @@
 import React, {Component} from 'react'
-import {Table, Descriptions} from 'antd'
+import {Table, Descriptions, Popconfirm, Icon, Modal} from 'antd'
+import Button from "../../../../../components/Button";
+import ActivityInfo from "./ActivityInfo";
 
-
-const columns = [
-    {title: '活动编号', align: 'center', dataIndex: 'eventId', key: 'eventId'},
-    {title: '活动名称', align: 'center', dataIndex: 'eventName', key: 'eventName'},
-    {title: '活动描述', align: 'center', dataIndex: 'eventDescription', key: 'eventDescription'},
-    {title: '开始时间', align: 'center', dataIndex: 'startTime', key: 'startTime'},
-    {title: '结束时间', align: 'center', dataIndex: 'endTime', key: 'endTime'},
-
-];
 
 function CustomExpandIcon(props) {
     let text;
@@ -27,10 +20,88 @@ function CustomExpandIcon(props) {
     );
 }
 
+
 class ManageActivity extends Component {
+
     constructor(props) {
-        super(props)
-    }
+        super(props);
+        this.state = ({
+            activityFormVisible: false,
+            activityFormConfirmLoading: false,
+            fields:{}//表单数据
+        })
+    };
+
+    //设置新增活动表格可见状态
+    showActivityForm = () => {
+        this.setState({
+            activityFormVisible: true
+        })
+    };
+
+
+
+    //提交表单
+    handleOk = () => {
+        this.setState({
+            activityFormConfirmLoading: true,
+        });
+        //TODO() 更改为调用上传数据成功后关闭
+        //  this.props.appendActivity();
+        setTimeout(() => {
+            this.setState({
+                activityFormVisible: false,
+                activityFormConfirmLoading: false,
+            });
+        }, 2000);
+
+    };
+
+    //取消更改
+    handleCancel = () => {
+        this.setState({
+            activityFormVisible: false
+        })
+    };
+
+    operation = () => {
+        return (
+            <div>
+                <span>操作</span>
+                <Icon style={{fontSize: '18px', marginLeft: '10px', color: '#FFEB9E'}} type="plus-circle"
+                      onClick={this.showActivityForm}/>
+                <Modal
+                    title="新活动信息"
+                    visible={this.state.activityFormVisible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    confirmLoading={this.state.activityFormConfirmLoading}
+                >
+
+                    <ActivityInfo />
+
+                </Modal>
+            </div>)
+    };
+
+    columns = [
+        {title: '活动编号', align: 'center', dataIndex: 'eventId', key: 'eventId'},
+        {title: '活动名称', align: 'center', dataIndex: 'eventName', key: 'eventName'},
+        {title: '活动描述', align: 'center', dataIndex: 'eventDescription', key: 'eventDescription'},
+        {title: '开始时间', align: 'center', dataIndex: 'startTime', key: 'startTime'},
+        {title: '结束时间', align: 'center', dataIndex: 'endTime', key: 'endTime'},
+        {
+            title: this.operation, align: 'center', dataIndex: 'delete',
+            render: (text, record) =>
+                this.props.activities.length >= 1 ? (
+                    <Popconfirm title="确认删除该活动吗？" onConfirm={() => this.props.deleteActivity(record.eventId)}>
+                        <Button type='yellow'>Delete</Button>
+                    </Popconfirm>
+                ) : null,
+        }
+
+    ];
+
 
     onExpand = (expanded, record) => {
         console.log('onExpand', expanded, record);
@@ -42,22 +113,24 @@ class ManageActivity extends Component {
                 <Descriptions
                     title={"优惠券名称： " + record.couponName}
                     border>
-                    <Descriptions.Item label="优惠券描述" span={3}>{record.couponDescription}</Descriptions.Item>
+                    <Descriptions.Item label="优惠券描述" >{record.couponDescription}</Descriptions.Item>
+                    <Descriptions.Item label="参与电影" span={2}>{record.moviesIncluded.join(',')}</Descriptions.Item>
                     <Descriptions.Item label="有效时长">{record.couponExpiration}</Descriptions.Item>
-                    <Descriptions.Item label="门槛">{record.couponThreshold + '元'}</Descriptions.Item>
+                    <Descriptions.Item label="使用门槛">{record.couponThreshold + '元'}</Descriptions.Item>
                     <Descriptions.Item label="折扣">{record.couponDiscount}</Descriptions.Item>
                     <Descriptions.Item label="图片">{record.couponPictureUrl}</Descriptions.Item>
                 </Descriptions>
             </div>
 
         )
-    }
+    };
+
 
     render() {
         const {activities} = this.props;
         return (
             <div>
-                <Table columns={columns} dataSource={activities}
+                <Table columns={this.columns} dataSource={activities}
                        expandIcon={CustomExpandIcon}
                        expandedRowRender={this.CustomExpandContent}
                        onExpand={this.onExpand}
--- src/routes/Managers/Activity/index.js
+++ src/routes/Managers/Activity/index.js
@@ -5,13 +5,14 @@ import WithSider from "../../../components/WithSider";
 import SendCoupon from "./components/SendCoupon";
 import ManageActivity from "./components/ManageActivity";
 import {Divider, Tabs} from "antd";
+import {addActivity, delActivity} from "../../../services/apiActivity";
 
 
 const {TabPane} = Tabs;
 
-const testActivityInfo = {
+const testActivityInfo = [{
     eventId: 1,
-    eventName: '春季外卖节',
+    eventName: '春季外卖节1',
     eventDescription: '春季外卖界活动描述',
     moviesIncluded: [1, 2, 3],
     startTime: '2019-04-21',
@@ -23,7 +24,37 @@ const testActivityInfo = {
     couponThreshold: 100, // 优惠券门槛
     couponExpiration: '三个月'
 
-};
+},
+    {
+        eventId: 2,
+        eventName: '春季外卖节2',
+        eventDescription: '春季外卖界活动描述',
+        moviesIncluded: [1],
+        startTime: '2019-04-21',
+        endTime: '2019-05-21',
+        couponName: '品质联盟',
+        couponDescription: '春节电影节优惠券111111111111111',
+        couponPictureUrl: '优惠券图片',
+        couponDiscount: 0.9, // 优惠券折扣
+        couponThreshold: 100, // 优惠券门槛
+        couponExpiration: '三个月'
+
+    },
+    {
+        eventId: 3,
+        eventName: '春季外卖节3',
+        eventDescription: '春季外卖界活动描述',
+        moviesIncluded: [1, 2, 3],
+        startTime: '2019-04-21',
+        endTime: '2019-05-21',
+        couponName: '品质联盟',
+        couponDescription: '春节电影节优惠券111111111111111',
+        couponPictureUrl: '优惠券图片',
+        couponDiscount: 0.9, // 优惠券折扣
+        couponThreshold: 100, // 优惠券门槛
+        couponExpiration: '三个月'
+
+    },];
 
 class Activity extends PureComponent {
     constructor(props) {
@@ -34,11 +65,31 @@ class Activity extends PureComponent {
     }
 
     componentWillMount() {
-        //TODO 调用接口19 获取当前所有活动信息
+        //TODO() 调用接口19 获取当前所有活动信息
         this.setState({
-            activities: Array(10).fill(testActivityInfo),
+            activities: testActivityInfo,
         })
     }
+
+    deleteActivity = eventId => {
+        console.log(eventId);
+        this.setState(
+            {
+                activities: this.state.activities.filter(activity => activity.eventId !== eventId)
+            }
+        );
+        //TODO() 调用接口21  删除优惠活动
+        // Bug：第一次点击删除无法删除
+        // delActivity(eventId);
+    };
+
+    appendActivity = activityInfo=>{
+        console.log(activityInfo);
+
+        //TODO() 调用接口20 新增优惠活动
+        // addActivity(activityInfo);
+    };
+
 
     render() {
         const {activities} = this.state;
@@ -47,7 +98,10 @@ class Activity extends PureComponent {
                 <div className={styles.wrapper}>
                     <Tabs type="card">
                         <TabPane tab="优惠活动" key="1">
-                            <ManageActivity activities={activities}/>
+                            <ManageActivity activities={activities}
+                                            deleteActivity={this.deleteActivity}
+                                            appendActivity={this.appendActivity}
+                            />
                         </TabPane>
                         <TabPane tab="发布优惠券" key="2">
                             <SendCoupon/>
