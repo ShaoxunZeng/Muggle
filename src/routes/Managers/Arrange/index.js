75a8818dbdab26e465e3a874e7eb148aa1f31f14
Gryffindor <838492657@qq.com>
2019-05-31 10:13:37 +0800

    feat: 修改了默认样式

--- src/routes/Managers/Arrange/index.js
+++ src/routes/Managers/Arrange/index.js
@@ -2,18 +2,36 @@ import { PureComponent } from "react";
 import React from "react";
 import styles from "./index.module.less";
 import WithSider from "../../../components/WithSider";
-import {DatePicker, Icon, Input, Tooltip} from "antd";
+import {DatePicker, Icon, Input, Select, Tooltip} from "antd";
+import TimeLine from "../../../components/TimeLine";
 
 class Arrange extends PureComponent {
   render() {
     return (
         <div className={styles.whole}>
-          Arrange
           <Input
               placeholder="Search Movies"
-              prefix={<Icon type="user" style={{ color: 'rgb(255,235,158)', }} />}
+              prefix={<Icon type="search" style={{ color: 'rgb(255,255,255)' }} />}
+              className={styles.input}
           />
-          <DatePicker className={styles.datepicker}/>
+          <div className={styles.underline}/>
+          <div className={styles['main-body']}>
+
+            <div className={styles.text}> 选择日期 </div>
+            <DatePicker className={styles.datepicker}/>
+            <div className={styles.text}> 选择影厅 </div>
+            <Select
+                showSearch
+                style={{ width: 200 }}
+                placeholder="Select a person"
+                optionFilterProp="children"
+            >
+              <Select.Option value="jack">Jack</Select.Option>
+              <Select.Option value="lucy">Lucy</Select.Option>
+              <Select.Option value="tom">Tom</Select.Option>
+            </Select>
+            <TimeLine intervals={[{startTime: '9:00', endTime: '10:00'}]}/>
+          </div>
         </div>
     )
   };
