94aca415ae9efe2ecfcf826c0deee83ede3afba5
Slytherin <1085684645@qq.com>
2019-06-03 20:48:28 +0800

    fix: 调整电影评论功能布局

--- src/routes/Users/UserHome/Message/index.js
+++ src/routes/Users/UserHome/Message/index.js
@@ -39,70 +39,6 @@ const testMsgList = [
         initTime: '2019-05-04',// 发送时间
         messageStatus: 1, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
         additionalMovieId: null
-    },
-    {
-        messageType: 1, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '您有想看电影上映了！', //消息标题
-        messageContent: '《复仇者联盟4》今日上映。', //消息内容
-        initTime: '2019-06-02',// 发送时间
-        messageStatus: 0, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: null
-    },
-    {
-        messageType: 0, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '被赠送优惠券', //消息标题
-        messageContent: '一张满300减30的优惠券已经送至您的卡包', //消息内容
-        initTime: '2019-06-03',// 发送时间
-        messageStatus: 0, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: null
-    },
-    {
-        messageType: 3, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '电影评价', //消息标题
-        messageContent: '看完《复联4》，快来评价吧！', //消息内容
-        initTime: '2019-06-03',// 发送时间
-        messageStatus: 1, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: 3
-    },
-    {
-        messageType: 4, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '您的会员卡可升级！', //消息标题
-        messageContent: '充值满500您的会员卡将会自动升级为白金会员卡。', //消息内容
-        initTime: '2019-05-04',// 发送时间
-        messageStatus: 1, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: null
-    },
-    {
-        messageType: 1, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '您有想看电影上映了！', //消息标题
-        messageContent: '《复仇者联盟4》今日上映。', //消息内容
-        initTime: '2019-06-02',// 发送时间
-        messageStatus: 0, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: null
-    },
-    {
-        messageType: 0, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '被赠送优惠券', //消息标题
-        messageContent: '一张满300减30的优惠券已经送至您的卡包', //消息内容
-        initTime: '2019-06-03',// 发送时间
-        messageStatus: 0, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: null
-    },
-    {
-        messageType: 3, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '电影评价', //消息标题
-        messageContent: '看完《复联4》，快来评价吧！', //消息内容
-        initTime: '2019-06-03',// 发送时间
-        messageStatus: 1, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: 2
-    },
-    {
-        messageType: 4, //<0: 被赠送优惠券>, <1: 想看电影上映>, <3: 邀请点评>, <4: 其他>,
-        messageTitle: '您的会员卡可升级！', //消息标题
-        messageContent: '充值满500您的会员卡将会自动升级为白金会员卡。', //消息内容
-        initTime: '2019-05-04',// 发送时间
-        messageStatus: 1, // <0: 未读>, <1: 已读> 第一次GET得到的状态是未读，之后GET得到的状态都是已读
-        additionalMovieId: null
     }
 ];
 
@@ -128,7 +64,7 @@ class Message extends PureComponent {
     showCommentForm = (movieId) => {
         this.setState({
             commentFormVisible: true,
-            commentMovieId:movieId
+            commentMovieId: movieId
         })
     };
     closeCommentInfoModal = () => {
@@ -147,7 +83,7 @@ class Message extends PureComponent {
             render: (record) => {
                 return (
                     record.messageType === 3 ?
-                        <Tag color={'yellow'} onClick={()=>this.showCommentForm(record.additionalMovieId)}>去评价</Tag> :
+                        <Tag color={'yellow'} onClick={() => this.showCommentForm(record.additionalMovieId)}>去评价</Tag> :
                         <Tag color={record.messageStatus === 0 ? 'gold' : 'grey'}>
                             {record.messageStatus === 0 ? '未读' : '已读'}
                         </Tag>
